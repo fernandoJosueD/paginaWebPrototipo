4ca147c87b9fe060cb66cb47703d091807f473e2
Fernando Dondiego <67974722+fernandoJosueD@users.noreply.github.com>
2021-06-10 10:20:53 -0600

    actualizacion de ventanas en productos y el catalogo

---
+++ js/productos.js
@@ -0,0 +1,113 @@
+/*-------------amarrado lizo ----------------------*/ 
+function amarrado (){
+var fotoa = document.getElementById("fotoa");
+var fotob = document.getElementById("fotob");
+var fotoc = document.getElementById("fotoc");
+var fotod = document.getElementById("fotod");
+var img = document.getElementById("imagen");
+
+fotoa.onmouseover = function(){
+    img.src = "../img/producto/amarrado (1).jpg";
+}
+fotob.onmouseover = function(){
+    img.src = "../img/producto/amarrado (2).jpg";
+}
+fotoc.onmouseover = function(){
+    img.src = "../img/producto/amarrado (3).jpg";
+}
+fotod.onmouseover = function(){
+    img.src = "../img/producto/amarrado (4).jpg";
+}
+}
+/*--------------amarrado con adorno lateral------------------*/
+
+function amarradoLateral (){
+var foto1 = document.getElementById("foto1");
+var foto2 = document.getElementById("foto2");
+var foto3 = document.getElementById("foto3");
+var foto4 = document.getElementById("foto4");
+var img1 = document.getElementById("imagen5");
+
+foto1.onmouseover = function(){
+    img1.src = "../img/producto/amarradoLateral (1).jpg";
+}
+foto2.onmouseover = function(){
+    img1.src = "../img/producto/amarradoLateral (2).jpg";
+}
+foto3.onmouseover = function(){
+    img1.src = "../img/producto/amarradoLateral (3).jpg";
+}
+foto4.onmouseover = function(){
+    img1.src = "../img/producto/amarradoLateral (4).jpg";
+}
+}
+/*--------botas --------------*/ 
+
+function botasNegras (){
+    var fotoa = document.getElementById("fotoa");
+var fotob = document.getElementById("fotob");
+var fotoc = document.getElementById("fotoc");
+var fotod = document.getElementById("fotod");
+var img = document.getElementById("imagen");
+
+fotoa.onmouseover = function(){
+    img.src = "../img/producto/bota (1).jpg";
+}
+fotob.onmouseover = function(){
+    img.src = "../img/producto/bota (2).jpg";
+}
+fotoc.onmouseover = function(){
+    img.src = "../img/producto/bota (3).jpg";
+}
+fotod.onmouseover = function(){
+    img.src = "../img/producto/bota (4).jpg";
+}
+
+}
+
+/*---------aladino Elastico */
+
+function aladinoElastico (){
+    var fotoa = document.getElementById("fotoa");
+var fotob = document.getElementById("fotob");
+var fotoc = document.getElementById("fotoc");
+/*var fotod = document.getElementById("fotod");*/
+var img = document.getElementById("imagen");
+
+fotoa.onmouseover = function(){
+    img.src = "../img/producto/aladitoElastico (1).jpg";
+}
+fotob.onmouseover = function(){
+    img.src = "../img/producto/aladitoElastico (2).jpg";
+}
+fotoc.onmouseover = function(){
+    img.src = "../img/producto/aladitoElastico (3).jpg";
+}
+/*fotod.onmouseover = function(){
+    img.src = "../img/producto/bota (4).jpg";
+}*/
+
+}
+/*-----amarrado pala liza */
+
+function amarradoNegro (){
+    var fotoa = document.getElementById("fotoa");
+var fotob = document.getElementById("fotob");
+var fotoc = document.getElementById("fotoc");
+var fotod = document.getElementById("fotod");
+var img = document.getElementById("imagen");
+
+fotoa.onmouseover = function(){
+    img.src = "../img/producto/amarNegro (1).jpg";
+}
+fotob.onmouseover = function(){
+    img.src = "../img/producto/amarNegro (2).jpg";
+}
+fotoc.onmouseover = function(){
+    img.src = "../img/producto/amarNegro (3).jpg";
+}
+fotod.onmouseover = function(){
+    img.src = "../img/producto/amarNegro (4).jpg";
+}
+
+}
