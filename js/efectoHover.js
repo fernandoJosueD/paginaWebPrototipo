5f2479e739225e49c5ab2b667934612437b4b72c
Fernando Dondiego <67974722+fernandoJosueD@users.noreply.github.com>
2021-06-16 00:38:27 -0600

    vista de producto modificada en dise;o

---
+++ js/efectoHover.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$("#link").hover(function(){
+		$("#link").css("background-color", "#2EBB30");
+		}, function(){
+			$("#link").css("background-color", "#C3C3C3");
+		});
+});
