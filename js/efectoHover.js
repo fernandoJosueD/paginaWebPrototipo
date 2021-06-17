2bef6f5be83bdf29351683894716dc7d0b07f516
Fernando Dondiego <67974722+fernandoJosueD@users.noreply.github.com>
2021-06-16 20:16:18 -0600

    se modifico la galeria y la vista de producto en ventana individual para ser especifico en el area de botones de redes sociales

--- js/efectoHover.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$("#link").hover(function(){
-		$("#link").css("background-color", "#2EBB30");
-		}, function(){
-			$("#link").css("background-color", "#C3C3C3");
-		});
-});
