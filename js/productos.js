194f5878ff87efb037b16f81e02cb43f29583e82
Fernando Dondiego <67974722+fernandoJosueD@users.noreply.github.com>
2021-06-18 00:06:52 -0600

    se modifico las imagenes de presentacion

--- js/productos.js
+++ js/productos.js
@@ -111,3 +111,25 @@ fotod.onmouseover = function(){
 }
 
 }
+/*nubok bege*/
+function  nubokbege () {
+    var fotoa = document.getElementById("fotoa");
+var fotob = document.getElementById("fotob");
+var fotoc = document.getElementById("fotoc");
+var fotod = document.getElementById("fotod");
+var img = document.getElementById("imagen");
+
+fotoa.onmouseover = function(){
+    img.src = "../img/producto/begeNubok (1).jpg";
+}
+fotob.onmouseover = function(){
+    img.src = "../img/producto/begeNubok (2).jpg";
+}
+fotoc.onmouseover = function(){
+    img.src = "../img/producto/begeNubok (3).jpg";
+}
+fotod.onmouseover = function(){
+    img.src = "../img/producto/begeNubok (4).jpg";
+}
+
+}
